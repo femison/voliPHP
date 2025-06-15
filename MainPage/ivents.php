7645bb997125681f90a5dd0a0950b53e4616c36b
femison <147845609+femison@users.noreply.github.com>
2025-06-15 23:30:09 +0200

    fix

--- MainPage/ivents.php
+++ MainPage/ivents.php
@@ -124,21 +124,25 @@
 </div>
 
 <footer style="z-index:999;">
-<section class="footer_section">
+  <section class="footer_section">
 
         
 
+        <div class="copyright-div">
+            <p class="footer_text">Copyright © 2025 Volunteering Organization</p>
+            <p class="footer_text">Все права защищены</p>
+            <a class="dog-link" href="../docs/Договор.pdf">Договор</a>
+        </div>
 
-
-<div class="email_div">
-    <p class="footer_text">Наши Социальные Сети</p>
-    <label>
-        <p>Вк</p>
-        <p>Телеграм</p>
-        
-    </label>
-</div>
-</section>
+        <div class="email_div">
+            <p class="footer_text">Наши Социальные Сети</p>
+            <label>
+                <p>Вк</p>
+                <p>Телеграм</p>
+                
+            </label>
+        </div>
+    </section>
 </footer>
 </body>
 </html>
